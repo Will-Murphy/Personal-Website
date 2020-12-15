84b44d12769025578db01a102c3077f4616ae548
Will Murphy <williammurphy9723@gmail.com>
2020-12-15 03:41:34 -0500

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,14 @@ import React from 'react'
 const About = () => (
     <span> 
         <p>
-            Hi! My name is Will. I am a senior at UMASS Amherst pursuing a career in software 
-            engineering. Solving problems involving logic, abstraction and mathematics is my passion - 
-            and theres nothing I love more than doing so through software that provides value 
-            to end users or allows for insights into the real world.
+            Hi! My name is Will. Solving problems involving logic, abstraction and mathematics 
+            is my passion - and theres nothing I love more than doing so through software that 
+            provides value to end users or allows for insights into the real world.
         </p>
         <p>
             I am most interested in full stack development. Although, I'd love to work 
             on either end of the stack. Aside from software, I'm interested in
-            volunteering, excercise, and finance.
+            volunteering, excercise, and personal finance.
     </p>
     <p>     Thanks for checking out my site! </p>
     <ul className="icons">
