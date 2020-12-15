60b8c0bd3e4c132669633858214571b9282048c6
Will Murphy <williammurphy9723@gmail.com>
2020-12-15 03:42:08 -0500

    Update Copyright, disclaimers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy;Will Murphy 2020. 
-                                 Design adapted from <a href="https://html5up.net">HTML5 UP</a> template. 
-                                 Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        
+        <p className="copyright"> 
+            &copy;Will Murphy 2021. 
+            Built with <a href="https://www.gatsbyjs.org/">Gatsby.js</a> and <a href="https://html5up.net">HTML5 UP. </a>
+            <span>Photo by <a href="https://unsplash.com/@dsc?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Davide Colonna. </a> </span>
+        </p>
+         
     </footer>
 )
 
